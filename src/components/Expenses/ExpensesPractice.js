3c7f58c0d64143f54d87fcb6963da02f5d79f387
pawel <pawel-czarnecki@o2.pl>
2021-08-23 21:59:31 +0100

    dodanie componentur wrapper 'Card.js' oraz zmiana funkcji na strzalkowe

---
+++ src/components/Expenses/ExpensesPractice.js
@@ -0,0 +1,9 @@
+function ExpensePractice(props){
+    return(
+        <div somedata={props.somedata}>
+            <h2>My own component</h2>
+        </div>
+    );
+}
+
+export default ExpensePractice;
